782e6ff10e17cc0ed5b2791a2a9e9f78dc955e52
GONZA\PC <gonza.cunet@outlook.com>
2024-12-11 22:41:04 -0300

    second commit

--- src/App.tsx
+++ src/App.tsx
@@ -5,13 +5,30 @@ import { SearchResultItem } from "./SearchResultItem";
 class App extends React.Component<any, any> {
   constructor(props) {
     super(props);
+    this.state = { results: [] };
   }
   render() {
-    const handleSearch = () => {};
+    const handleSearch = async (search) => {
+      const res = await fetch(
+        "https://api.mercadolibre.com/sites/MLA/search?q=" + search
+      );
+      const data = await res.json();
+      this.setState({ results: data.results });
+    };
+    console.log(this.state);
     return (
       <div>
-        <SearchForm onSearch={handleSearch}></SearchForm>
-        <SearchResultItem></SearchResultItem>
+        <SearchForm onSearch={handleSearch} />
+        {this.state.results.map((r) => {
+          return (
+            <SearchResultItem
+              title={r.title}
+              price={r.price}
+              thumbnail={r.thumbnail}
+              key={r.id}
+            />
+          );
+        })}
       </div>
     );
   }
--- src/SearchResultItem.tsx
+++ src/SearchResultItem.tsx
@@ -3,7 +3,13 @@ import React from "react";
 
 class SearchResultItem extends React.Component<any, any> {
   render() {
-    return <div>Search result Item</div>;
+    return (
+      <div>
+        <h1>{this.props.title}</h1>
+        <h2>{this.props.price}</h2>
+        <img src={this.props.thumbnail} alt="" />
+      </div>
+    );
   }
 }
 export { SearchResultItem };
